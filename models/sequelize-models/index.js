21ff47ef6b0304afd24bbd67a4f9701b0ffa47aa
Jesus Salomon <salomonj1121@gmail.com>
2022-08-13 20:48:54 -0500

    Final Solution

--- models/sequelize-models/index.js
+++ models/sequelize-models/index.js
@@ -1,5 +1,6 @@
 const db = require('./database');
 const Sequelize = require('sequelize');
+const todos = require('/Users/salomonj/Checkpoint-Express-Sequelize-B/models/express-models/todos.js');
 
 // Make sure you have `postgres` running!
 
@@ -33,6 +34,63 @@ const Owner = db.define('Owner', {
 Task.belongsTo(Owner);
 Owner.hasMany(Task);
 
+Task.prototype.assignOwner = async function (person) {
+  const meAssign = this.setOwner(person);
+  return meAssign;
+};
+
+Owner.getOwnersAndTasks = async function () {
+  const wholeLotOfStuff = await this.findAll({ include: Task });
+  return wholeLotOfStuff;
+};
+
+Owner.prototype.getIncompleteTasks = async function () {
+  let myBad = await this.getTasks();
+  return myBad.filter((task) => task.complete === false);
+};
+
+Owner.beforeDestroy((person) => {
+  if (person.name === 'Grace Hopper') {
+    throw new Error('Woah calm down cowboy');
+  }
+});
+
+Task.clearCompleted = async function () {
+  const woahCompleted = await Task.destroy({
+    where: { complete: true },
+  });
+  return woahCompleted;
+};
+
+Task.completeAll = async function () {
+  const woahCleared = await this.update(
+    { complete: true },
+    {
+      where: {
+        complete: false,
+      },
+    }
+  );
+  return woahCleared;
+};
+
+Task.prototype.getTimeRemaining = function () {
+  if (!this.due) {
+    return Infinity;
+  } else {
+    return this.due - Date.now();
+  }
+};
+
+Task.prototype.isOverdue = function () {
+  if (this.due < Date.now() && this.complete === true) {
+    return false;
+  } else if (this.due > Date.now()) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 //---------^^^---------  your code above  ---------^^^----------
 
